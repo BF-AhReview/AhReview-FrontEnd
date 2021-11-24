51a3fdfb4e7a79bb326257e27e32e9de1f74a432
JeongTaehwan <wjdxoghks11222@naver.com>
2021-11-24 23:10:35 +0900

    [Add] : Text & SVG

--- src/components/Content1.tsx
+++ src/components/Content1.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as s from './Content1_s';
+import * as i from '../assets/index';
 
 const Content1: React.FC = () => {
     return (
@@ -11,7 +12,20 @@ const Content1: React.FC = () => {
                     <li className="register">등록하기</li>
                 </s.Btns>
             </s.Header>
-        </s.Contents>
+            <s.Main>
+                <s.Wrapper>
+                    <s.Texts>
+                        <p className="MainText">Ah! Review</p>
+                        <div className="SubTexts">
+                            <div className="SubText"><p className="BoldText">Ah! Review</p>는 소상공인 대상의 악성리뷰,</div>
+                            <div className="SubText">악성댓글으로 부터 보호해주는 <p className="BoldTextTwo"> 플랫폼</p>입니다</div>
+                        </div>
+                    </s.Texts>
+                    <i.PlayBtn className="PlayBtn" />
+                </s.Wrapper>
+                <i.Feedback className="FeedBack" />
+            </s.Main>
+        </s.Contents >
     );
 }
 
--- src/components/Content1_s.tsx
+++ src/components/Content1_s.tsx
@@ -7,7 +7,7 @@ export const Contents = styled.div`
 `;
 
 export const Header = styled.div`
-        display: flex;
+    display: flex;
     align-items: center;
     justify-content: center;
 `;
@@ -24,11 +24,54 @@ export const Btns = styled.ul`
 
     .login {
         font-weight: bold;
+        font-size: 18px;
         color: white;
     }
     .register {
         margin-left: 2.5vh;
         font-weight: bold;
+        font-size: 18px;
         color: white;
+    }
+`;
+
+export const Main = styled.div`
+    .FeedBack {
+        margin-left: 100vh;
+        margin-top: -36vh;
+    }
+`;
+
+export const Wrapper = styled.div`
+    margin-bottom: -10vh; 
+
+    .PlayBtn {
+        margin-left: 40vh;
+    }
+`;
+
+export const Texts = styled.div`
+    margin-left: 40vh;
+
+    .MainText {
+        font-size: 8vh;
+        font-weight: bold;
+        color: #52734D;
+    }
+    .SubTexts {
+        margin-top: -2rem;
+    }
+    .SubText {
+        display: flex;
+        font-size: 20px;
+        font-weight: bold;
+    }
+    .BoldText {
+        color: #52734D;
+        margin-top : -0.1rem;
+    }
+    .BoldTextTwo {
+        color: #52734D;
+        margin-top: -0.05rem;
     }
 `;
