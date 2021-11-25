b389536d6c88941facd113d0ed535ebc97c5ac77
JeongTaehwan <wjdxoghks11222@naver.com>
2021-11-25 15:18:46 +0900

    [Add] : 네이버 로그인 받아오는 중

--- src/components/Content1/Content1.tsx
+++ src/components/Content1/Content1.tsx
@@ -1,35 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as s from './styled';
 import * as i from '../../assets'
 
 const Content1: React.FC = () => {
-    const [showModal, setShowModal] = useState(false);
-    const onModal = () => {
-        setShowModal(true);
+    // const [showModal, setShowModal] = useState(false);
+    // const onModal = () => {
+    //     setShowModal(true);
+    // }
+
+    // const handlePayModalOff = (e: any) => {
+
+    //     const clicked = e.target.closest('.paymodal');
+
+    //     if (clicked) return;
+
+    //     else {
+    //         setShowModal(false);
+    //     }
+    // };
+
+    const { naver } = window as any;
+
+    function Login(props: any) {
+        const initializeNaverLogin = () => {
+            const naverLogin = new naver.LoginWithNaverId({
+                clientId: 'S0OAHuxMBd6gwiSnnys3',
+                callbackUrl: 'http://localhost:3000/',
+                isPopup: true, // popup 형식으로 띄울것인지 설정
+                loginButton: { color: 'white', type: 1, height: '47' }, //버튼의 스타일, 타입, 크기를 지정
+            });
+            naverLogin.init();
+        };
+        useEffect(() => {
+            initializeNaverLogin();
+        }, []);
     }
-
-    const handlePayModalOff = (e: any) => {
-
-        const clicked = e.target.closest('.paymodal');
-
-        if (clicked) return;
-
-        else {
-            setShowModal(false);
-        }
-    };
-
 
     return (
         <s.Contents>
             <s.Header>
                 <s.Logo>Ah! Review</s.Logo>
-                <s.Btns>
-                    <li className="login" onClick={onModal}>로그인</li>
+                <s.Btns id="naverIdLogin" >
+                    <li className="login" onClick={Login}>로그인</li>
                     <li className="register">등록하기</li>
                 </s.Btns>
             </s.Header>
-            {
+            {/* {
                 showModal ?
                     <s.Background
                         className="paymodalWrapper"
@@ -41,7 +57,7 @@ const Content1: React.FC = () => {
                         </s.ModalContainer>
                     </s.Background>
                     : null
-            }
+            } */}
             <s.Main>
                 <s.Wrapper>
                     <s.Texts>
