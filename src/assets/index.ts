d9a49f378953dd4e55db151a0c1b3588dc89fe6d
LeeJaeSeoung <wotjd0676@gmail.com>
2021-11-25 22:01:53 +0900

    [Add] : PostStore
    가게 등록하기 퍼블리싱 완료

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,3 +4,4 @@ export { default as PlayBtn } from './PlayBtn';
 export { default as Business } from './Business';
 export { default as Github } from './Github';
 export { default as Naver } from './Naver';
+export { default as dodbogi } from './dodbogi.svg';
--- src/components/PostStore/PostStore.tsx
+++ src/components/PostStore/PostStore.tsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
+import { dodbogi } from "../../assets";
 import * as S from "./styled";
 
 const PostStore = () => {
+
+  const [errorMessage,setErrorMessage] = useState("");
+
   return (
     <S.Background>
-
-
+      <S.Container>
+        <S.Title>등록</S.Title>
+        <S.InputContainer>
+          <S.Input placeholder="가게 이름을 입력하세요." />
+          <S.Dodbogi src={dodbogi} />
+        </S.InputContainer>
+        <S.ErrorMessage>&nbsp;</S.ErrorMessage>
+        <S.InputContainer>
+          <S.Input placeholder="가게 위치를 입력하세요." />
+          <S.Dodbogi src={dodbogi} />
+        </S.InputContainer>
+        <S.ErrorMessage>&nbsp;</S.ErrorMessage>
+        <S.PostButton>등록하기</S.PostButton>
+      </S.Container>
     </S.Background>
   );
 };
